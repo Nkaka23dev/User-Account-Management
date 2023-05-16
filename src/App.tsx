fe600a5ee7bb6ae4bccd6912a619b0c5a86bcc8a
Nkaka23dev <nkakaeric96@gmail.com>
2023-05-16 16:26:56 +0300

    feat: finish login form design and validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import Login from "./components/Login";
+import PageLayout from "./pages/PageLayout";
 import Register from "./components/Register";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<PageLayout />}>
+      <Route index element={<Register />} />
+      <Route path="login" element={<Login />} />
+    </Route>
+  )
+);
 
 export default function App() {
   return (
-    <Register />
+    <RouterProvider router={router} />
   )
 }
